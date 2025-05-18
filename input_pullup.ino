d1b1d82dfacb3777de9def28021ec4872bbf4062
Anas Mahmoud <ansas4565@gmail.com>
2025-05-18 12:06:23 +0300

    Update input_pullup.ino

--- input_pullup.ino
+++ input_pullup.ino
@@ -1,11 +1,28 @@
-# قم بتوصيل السلك الاخر مع gnd و ليس اي دبوس اخر 
+// قم بتوصيل السلك الآخر مع GND وليس أي دبوس آخر
 #define INPUT_PIN 8 
+#define RELAY_PIN 4
 
 void setup() {
   Serial.begin(9600);
-  pinMode(INPUT_PIN, INPUT_PULLUP);
+
+  pinMode(INPUT_PIN, INPUT_PULLUP);  // إدخال مع مقاومة سحب داخلية
+  pinMode(RELAY_PIN, OUTPUT);        // مخرج للمرحل
+
+  digitalWrite(RELAY_PIN, LOW);      // افتراضياً: المرحل متوقف
 }
 
 void loop() {
-  Serial.println(digitalRead(INPUT_PIN));
+  int inputState = digitalRead(INPUT_PIN);
+
+  Serial.println(inputState);  // طباعة حالة الدبوس 8
+
+  if (inputState == LOW) {
+    // الدبوس موصول بالأرض → نشغّل المرحل
+    digitalWrite(RELAY_PIN, HIGH);
+  } else {
+    // الدبوس غير موصول → نوقف المرحل
+    digitalWrite(RELAY_PIN, LOW);
+  }
+
+  delay(200); // تأخير بسيط لتقليل سرعة الطباعة
 }
